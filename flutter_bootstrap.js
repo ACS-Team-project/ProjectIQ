85ab54a96b70fde1b8de3e0639488f518e5ae6d6
Bhromor Gayen <v-bgayen@microsoft.com>
2025-04-30 23:59:44 +0100

    Flutter web deploy....8

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4166846574"
+    serviceWorkerVersion: "1036556833"
   }
 });
